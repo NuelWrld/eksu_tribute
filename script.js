d7a885562f06611d09aa4dd4caefc5fc42d42641
NuelWrld <emmygram911@gmail.com>
2023-11-21 12:05:56 +0100

    fix gallery button

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const slider = document.querySelector('.slider');
+    const slides = document.querySelectorAll('.slide');
+    const prevBtn = document.querySelector('.prevBtn');
+    const nextBtn = document.querySelector('.nextBtn');
+  
+    let currentIndex = 0;
+  
+    function updateSlider() {
+      slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+  
+    function showSlide(index) {
+      currentIndex = index;
+      updateSlider();
+    }
+  
+    prevBtn.addEventListener('click', () => {
+      currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+      updateSlider();
+    });
+  
+    nextBtn.addEventListener('click', () => {
+      currentIndex = (currentIndex + 1) % slides.length;
+      updateSlider();
+    });
+  });
+  
+
 const form = document.querySelector('.form')
 
 function sendMessage() {
